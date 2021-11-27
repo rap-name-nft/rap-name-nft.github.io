c33a6e65fa13b42e95f0bf0de7271d1d4ee56b16
akgrove <akgrove@protonmail.com>
2021-11-26 21:34:31 -0800

    update rinkeby

--- rap-name-nft/packages/contracts/src/addresses.js
+++ rap-name-nft/packages/contracts/src/addresses.js
@@ -3,7 +3,7 @@
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
   ropstenRapNameNft: "0x4fe6c7b51360073d675ad6f7912371a8360bae0a",
-  rinkebyRapNameNft: "0x15785aCFD609b88737A4816c87C7fe379c9841a7",
+  rinkebyRapNameNft: "0x13F6e9d9286a08e05ccb596c10BC1b403D0A2005",
   mainnetRapNameNft: "0x0"
 };
 
